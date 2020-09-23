46145a09a1175cd7cbabbe1c57d14555a71a3e24
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-09-23 18:06:02 -0400

    see TODO in Fetch.js

--- src/Components/Fetch.js
+++ src/Components/Fetch.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Fetch = ({ stateAbrv }) => {
-    
+
     const [data, setData] = useState([]);
 
     const url = `https://developer.nps.gov/api/v1/parks?stateCode=${stateAbrv}&api_key=`;
@@ -20,13 +20,30 @@ const Fetch = ({ stateAbrv }) => {
 
     }, [stateAbrv]);
 
+    const handleClick = (arg) => {
+        console.log(arg.activities)
+        //TO DO...
+        /*
+        set to State, pass state to activities component,
+        pull out 'name' values and display in a map
+        */
+    }
 
     return (
         <div>
+            <h3>Parks</h3>
             <ul>
                 {data.map(park => (
                     <li key={park.fullName}>
-                        {park.fullName}
+                        Park Name: {park.fullName} Activities: <button
+                            onClick={() => handleClick(park)}
+                            style={{
+                                borderRadius: '50%',
+                                background: 'blue',
+                                color: 'white'
+                            }}>
+                            {park.activities.length}
+                        </button>
                     </li>
                 ))}
             </ul>
